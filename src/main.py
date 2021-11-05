9cd5e281c62e28e357c8128fec9c5a4fdd710bd6
Krlos160287 <martigracia2018@gmail.com>
2021-11-05 20:33:01 +0000

    Finished

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, Person
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -19,28 +18,29 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
 @app.route('/all', methods=['GET'])
-def getAll():
-    response_body = Person.getAll()
-    return jsonify(response_body), 200
+def get_persons():
+    persons = Person.get_persons()
+    return jsonify(persons), 200
+@app.route('/member/<int:id>', methods = ['GET'])
+def get_person_id(id):
+    person = Person.get_person_id(id)
+        
+    return jsonify(person), 200
 
 @app.route('/person', methods=['POST'])
 def create_person():
     body = request.get_json()
     Person.create_person(body['name'], body['last_name'], body['rol'], body['age'], body['father_id'], body['mother_id'])
-    return jsonify({"msg":"ok"}),200
-
+    return jsonify({"msg": "ok"}), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,32 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
-
 class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    last_name = db.Column(db.String(250), nullable=False)
-    rol = db.Column(db.String(250), nullable=False)
+    __tablename__ = 'person'
+    id = db.Column(db.String(400), primary_key=True, nullable=True)
+    name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    rol = db.Column(db.String(120), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    father_id = db.Column(db.Integer, db.ForeignKey('person.id'))
-    mother_id = db.Column(db.Integer, db.ForeignKey('person.id'))
-
+    father_id = db.Column(db.String(400), db.ForeignKey ('person.id'), nullable= True)
+    mother_id = db.Column(db.String(400), db.ForeignKey ('person.id'), nullable = True)
     def __repr__(self):
-        return '<Person %r>' % self.name
-
+        return '<Person %r>' % self.id
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "age": self.age,
+            "last_name": self.last_name,
+            "rol":self.rol,
+            "age":self.age,
+            "father_id":self.father_id,
+            "mother_id":self.mother_id
+                
             # do not serialize the password, its a security breach
         }
-    def getAll():
-        people = Person.query.order_by(Person.age.desc())
-        people = list(map(lambda person: person.serialize(), people))
-        return people
+    def get_persons():
+        persons = Person.query.order_by(Person.age.desc())
+        persons = list(map(lambda person: person.serialize(), persons))
+        return persons
     
-    def create_person(name,last_name,rol,age,father_id,mother_id):
+    def get_person_id(id):
+        Me = Person.query.get(id)
+        Father = Person.query.filter_by(id=Me.father_id).first()
+        if Father is None:
+            Father = []
+        Mother = Person.query.filter_by(id=Me.mother_id).first()
+        if Mother is None:
+            Mother = []
+        Child = Person.query.filter( (Person.father_id == id) | (Person.mother_id == id) ).first()
+        if Child is None:
+            Child = []
+        family = {
+            "father": Person.serialize(Father),
+            "mother": Person.serialize(Mother),
+            "me": Person.serialize(Me),
+            "child": Person.serialize(Child)
+
+        }
+
+
+    def create_person(name, last_name, rol, age, father_id, mother_id):
         person = Person(name=name, last_name=last_name, rol=rol, age=age, father_id=father_id, mother_id=mother_id)
         db.session.add(person)
         db.session.commit()
